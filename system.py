25150a8b3a6f88d43931baa9224297e57c7283f4
Christian Bueche <buechech@umich.edu>
2017-04-13 20:53:15 -0400

    system.py

--- system.py
+++ system.py
@@ -1,9 +1,17 @@
 import sys
 import os
 import re
-from math import log
+from math import log10
 import codecs
 from itertools import izip
+from urllib2 import Request, urlopen, URLError
+from TwitterAPI import TwitterAPI
+import json
+
+api = TwitterAPI('6hckfkSBDEUJRxPATcCtcsdOp',
+            		'GduOLco0wL1pOTboeClVPmZe8PPXEYD0VKnmKWpb2of7UNTbrY',
+                    '399687253-xb9c6wdOQMBU8K2Jk3utxDLuqC21qRLTajkV9iel',
+                    'bXdT9ejraFk6O9bNExA2sD71jgixMFby8nqzQwfsTLXKa')
 
 # Age Ranges	: dictionary key
 # 	19- 		: 15
@@ -21,27 +29,32 @@ from itertools import izip
 def create_dictionaries():
 	uniword = {}
 	biword = {}
+	syllab_avg = {}
+	total_tweets = {}
 
 	age = 15
 	while age < 70:
 		uniword[age] = {}
 		biword[age] = {}
+		syllab_avg[age] = 0.0
 		age += 5
 
-	return uniword, biword
+	return uniword, biword, syllab_avg
 
-#input: array of all tweets
-#input: age of celebrity
-#input: uniword dict
-#input: biword dict
-def train_system(tweets, age, uniword, biword):
-	#normalize age
+def normalize_age(age):
 	age /= 5
 	age *= 5
 	if age < 15:
 		age = 15
 	if age > 65:
 		age = 65
+	return age
+
+#input: array of all tweets
+#input: age of celebrity
+#input: uniword dict
+#input: biword dict
+def train_system(tweets, age, uniword, biword):
 
 	#cycle through tweets and add to uniword and biword dictionaries
 	for tweet in tweets:
@@ -58,7 +71,83 @@ def train_system(tweets, age, uniword, biword):
 
 	return
 
-# def estimate_age(tweets, uniword, biword):
+def train_system_syllables(tweets):
+
+	syll_count = 0.0
+	word_count = 0.0
+
+	for tweet in tweets:
+
+		for word in tweet.split():
+			request = Request("https://api.datamuse.com/words?sp=" + word + "&md=s")
+
+			try:
+				response = urlopen(request)
+				results = response.read()
+				dict_format = json.loads(results)
+				if len(dict_format) > 0:
+					syll = dict_format[0]["numSyllables"]
+				else:
+					syll = 1.666666666	#average syll/word in Eng lang.
+			except:
+				print word
+				print "fuck"
+				syll = 1.666666666
+
+			word_count += 1.0
+			syll_count += syll
+
+	return syll_count/word_count
+
+def load_dictionaries_from_files():
+	with open("saved_dictionaries/uniword") as file:
+		data = file.read()
+	uniword = json.loads(data)
+	with open("saved_dictionaries/biword") as file:
+		data = file.read()
+	biword = json.loads(data)
+	with open("saved_dictionaries/syllab_avg") as file:
+		data = file.read()
+	syllab_avg = json.loads(data)
+
+	return biword, uniword, syllab_avg
+
+def demo(username, uniword, biword, syllab_avg):
+	tweets = []
+
+	try:
+		results = api.request('statuses/user_timeline', {'screen_name': username, 'count': 40, 'include_rts': False})
+		for item in results:
+			tweets.append(item['text'])
+	except:
+		print "You either typed in your username wrong or you don't have a twitter\n"
+		return
+
+	print tweets
+	if len(tweets) < 1:
+		return
+	estimate_age(tweets, uniword, biword, syllab_avg)
+
+
+def estimate_age(tweets, uniword, biword, syllab_avg):
+
+	probabilities = {}
+	prev_word = ""
+
+	for word in tweets:
+		for age in uniword:
+			if prev_word != "":
+				if age in probabilities:
+					probabilities[age] += log10((float(biword[age].get(prev_word + " " + word, 0) + 1) / (uniword[age].get(prev_word, 0) + max(len(uniword[age]), 1))))
+				else:
+					probabilities[age] = log10((float(biword[age].get(prev_word + " " + word, 0) + 1) / (uniword[age].get(prev_word, 0) + max(len(uniword[age]), 1))))
+		prev_word = word
+
+	print probabilities
+	print max(probabilities) 
+
+
+# def test_system(tweet, uniword, biword, syllab_avg):
 # 	vocabs = list()
 # 	for i in range(0, len(lang_list)):
 # 		count = len(unigrams[i].keys())
@@ -100,39 +189,40 @@ def train_system(tweets, age, uniword, biword):
 
 if __name__ == '__main__':
 
-	#initialize dictionaries with empty dictionaries for each age range
-	uniword, biword = create_dictionaries()
+	# #initialize dictionaries with empty dictionaries for each age range
+	# uniword, biword, syllab_avg = create_dictionaries()
 
-	#loop through tweets in and train system on each tweet
-	for file in os.listdir("tweets/"):
-		file = "tweets/" + file
-		with open(file) as file:
-			tweets = []
-			age = 0
-			first_line = True
-			for line in file:
-				if first_line:
-					age = int(line)
-					first_line = False
-				else:
-					tweets.append(line)
-
-		#train system 
-		train_system(tweets, age, uniword, biword)
-	
-
-	print uniword
-	print "\n"
-	print biword
-
-
-
-	# test = str(sys.argv[1])
-	# i = 0
-	# with codecs.open('languageIdentification.output', 'w') as output:
-	# 	with codecs.open(test, 'r') as file:
+	# #loop through tweets in and train system on each tweet
+	# for file in os.listdir("tweets/"):
+	# 	file = "tweets/" + file
+	# 	with open(file) as file:
+	# 		tweets = []
+	# 		age = 0
+	# 		first_line = True
 	# 		for line in file:
-	# 			output.write(str(i + 1) + " " + identifyLanguage(line, langs, unigrams, bigrams) + "\n")
-	# 			i += 1
+	# 			if first_line:
+	# 				age = normalize_age(int(line))
+	# 				first_line = False
+	# 			else:
+	# 				tweets.append(line)
 
+	# 	#train system -> loop over age-tweets dict and train sys
+	# 	train_system(tweets, age, uniword, biword)
+	# 	syllab_avg[age] = train_system_syllables(tweets)
+
+	# with codecs.open('saved_dictionaries/uniword', 'w') as output:
+	# 	dumped = json.dumps(uniword)
+	# 	output.write(dumped)
+	# with codecs.open('saved_dictionaries/biword', 'w') as output:
+	# 	dumped = json.dumps(biword)
+	# 	output.write(dumped)
+	# with codecs.open('saved_dictionaries/syllab_avg', 'w') as output:
+	# 	dumped = json.dumps(syllab_avg)
+	# 	output.write(dumped)
+
+
+	uniword, biword, syllab_avg = load_dictionaries_from_files()
+	while 1:
+		username = raw_input("What's your twitter handle?\n")
+		demo(username, uniword, biword, syllab_avg)
 
